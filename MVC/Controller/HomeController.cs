fa85672f5b4df4c7400dad371d34c6e3e18c79b1
MarmikKalariya33 <marmikkalariya33@gmail.com>
2026-07-02 22:29:42 +0530

    fghgh

---
+++ MVC/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
