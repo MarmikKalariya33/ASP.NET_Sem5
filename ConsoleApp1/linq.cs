01bba56aac98bad3f4888616d4b20b1a0a776098
MarmikKalariya33 <marmikkalariya33@gmail.com>
2026-07-02 10:37:48 +0530

    Add ConsoleApp1

---
+++ ConsoleApp1/linq.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class linq
+    {
+        public static void run()
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+
+            // 1. where()
+            Console.WriteLine("where ( grater then 25 )");
+            var result1 = numbers.Where(n => n > 25);
+            foreach (var number in result1)
+            {
+                Console.WriteLine(number);
+            }
+
+            // 2. select()
+            Console.WriteLine("select ( multiply by 2 )");
+            var result2 = numbers.Select(n => n * 2);
+            foreach (var number in result2)
+            {
+                Console.WriteLine(number);
+            }
+
+            // 3. ordereby()
+            Console.WriteLine("orderby");
+            var result3 = numbers.OrderBy(n => n);
+            foreach (var number in result3)
+            {
+                Console.WriteLine(number);
+            }
+
+            // 4. orderbydescending()
+            Console.WriteLine("orderbydescending");
+            var result5 = numbers.OrderByDescending(n => n);
+            foreach (var number in result5)
+            {
+                Console.WriteLine(number);
+            }
+
+            // 6. firstordefault()
+            Console.WriteLine("first or default");
+            Console.WriteLine(numbers.FirstOrDefault());
+
+            //7. Any()
+            Console.WriteLine("Any");
+            Console.WriteLine(numbers.Any());
+
+            //8. count()
+            Console.WriteLine("count");
+            Console.WriteLine(numbers.Count());
+
+            //9. sum ()
+            Console.WriteLine("sum");
+            Console.WriteLine(numbers.Sum());
+
+            //10 take()
+            Console.WriteLine("take first 3 number");
+            var result10 = numbers.Take(3);
+            foreach (var number in result10)
+            {
+                Console.WriteLine(number);
+            }
+
+            //11. skip()
+            Console.WriteLine("skip first 3");
+            var result11 = numbers.Skip(3);
+            foreach (var number in result11)
+            {
+                Console.WriteLine(number);
+            }
+
+            //12. distinct()
+            Console.WriteLine("distinct");
+            var result12 = numbers.Distinct();
+            foreach (var number in result12)
+            {
+                Console.WriteLine (number);
+            }
+
+            //13. contains()
+            Console.WriteLine("contains 30 exist or not ");
+            bool result13 = numbers.Contains(30);
+            Console.WriteLine(result13);
+
+            //14. tolist()
+            Console.WriteLine("to list convert into list ");
+            List<int> result14 = numbers.ToList();
+            foreach (var number in result14)
+            {
+                Console.WriteLine(number);
+            }
+
+            //15. first()
+            Console.WriteLine("first");
+            int result15 = numbers.First();
+            Console.WriteLine(result15);
+
+            //16. last()
+            Console.WriteLine("last");
+            int result16 = numbers.Last();
+            Console.WriteLine(result16);
+
+            //17. single()
+            Console.WriteLine("single");
+            int result17 = numbers.Single(n => n == 30);
+            Console.WriteLine(result17);
+
+            //18. lastordefault()
+            Console.WriteLine("last or default");
+            int result18 = numbers.LastOrDefault();
+            Console.WriteLine(result18);
+        }
+    }
+}
+
---
+++ ConsoleApp1/linq2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class linq2
+    {
+       public static void run()
+        {
+            int[] number = { 10, 20, 30, 40, 50, };
+            string[] names = { "ram" , "sita" };
+
+            //All
+            Console.WriteLine("All number are grater then 5");
+            Console.WriteLine(number.All(n => n > 5));
+
+            //Min 
+            Console.WriteLine("Minimum numbber");
+            Console.WriteLine(number.Min());
+
+            //Max
+            Console.WriteLine("Maximum number");
+            Console.WriteLine(number.Max());
+
+            //Average
+            Console.WriteLine("Average number");
+            Console.WriteLine(number.Average());
+
+            //SingleorDefault
+            Console.WriteLine("singleordefault");
+            int result = number.SingleOrDefault(n => n == 30);
+            Console.WriteLine(result); // match number then 0 return and multi match return exception 
+
+            //SelectMany
+            Console.WriteLine("select many");//string form data use here 
+            var result1 = names.SelectMany(names => names);
+            foreach (var name in result1)
+            {
+                Console.WriteLine(name);
+
+            }
+
+            //join 
+            var student = new List<(int id, string name)>
+            {
+                (1,"ram"),
+                (2,"sita"),
+                (3,"gita")
+            };
+            var mark = new List<(int stuid, int mark)>
+            {
+                (1, 90),
+                (2, 80),
+                (3, 70)
+            };
+            var result7 = student.Join(
+                mark,
+                s => s.id,
+                m => m.stuid,
+                (s, m) => new { s.name, m.mark });
+            foreach (var r in result7)
+            {
+                Console.WriteLine($"{r.name} - {r.mark}");
+            }
+        }
+}
+}
