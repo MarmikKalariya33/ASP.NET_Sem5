8ed50381cbd60df83f2305e6e9398389e5c4a2e2
MarmikKalariya33 <marmikkalariya33@gmail.com>
2026-07-19 21:34:33 +0530

    All API  Convert to IServices

--- api_learn/Services/IProductService.cs
+++ api_learn/Services/IProductService.cs
@@ -5,5 +5,11 @@ namespace api_learn.Services
     public interface IProductService
     {
        Task <List<ProductMaster>> GetAllProducts();
+
+       Task<ProductMaster> CreateNewProduct(ProductMaster obj);
+
+       Task<ProductMaster> UpdateProduct(ProductMaster obj);
+
+       Task<ProductMaster> DeleteProduct(int Pro_Id);
     }
 }
